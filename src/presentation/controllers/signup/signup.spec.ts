49df170d2023b15cc076c7658f884ce403a2908d
Henrique Albuquerque <henrique_barretto@outlook.com>
2021-02-16 21:02:34 -0300

    test: ensure SignUpController return 200 if valid account data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -25,8 +25,8 @@ const makeAddAccount = (): AddAccount => {
     add (account: AddAccountModel): AccountModel {
       const fakeAccount: AccountModel = {
         id: 'valid_id',
-        name: 'any_name',
-        email: 'valid_email@email.com',
+        email: 'valid_email@mail.com',
+        name: 'valid_name',
         password: 'valid_password'
       }
       return fakeAccount
@@ -209,5 +209,25 @@ describe('SignUp Controlller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(500)
     expect(httpResponse.body).toEqual(new ServerError())
+  })
+  test('Should return 200 if valid data is provided', () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        email: 'valid_email@mail.com',
+        name: 'valid_name',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(200)
+    expect(httpResponse.body).toEqual({
+      id: 'valid_id',
+      email: 'valid_email@mail.com',
+      name: 'valid_name',
+      password: 'valid_password'
+    })
   })
 })
